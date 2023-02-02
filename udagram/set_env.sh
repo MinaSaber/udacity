340700af89a22a93c5f3845c8e8db0f72596e453
MinaSaber <mina.saper.12@gmail.com>
2023-02-02 22:54:36 +0200

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=password123
+export POSTGRES_HOST=database-1.cwho5aqawcrk.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=http://saber-udagram.s3-website-us-east-1.amazonaws.com
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
